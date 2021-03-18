f828296e8463deb73fd596cf6be7189bfd4ce683
KPMGE <jesuskevin254@gmail.com>
2021-03-18 13:22:50 -0300

    update main

--- src/main.c
+++ src/main.c
@@ -5,13 +5,17 @@
 int main(void) {
   FILE *myFile = fopen("testFile.csv", "w");
 
-  Rational_t *num1 = createRationalNumber(1, 4);
+  Rational_t *num1 = createRationalNumber(1, 2);
   Rational_t *num2 = createRationalNumber(4, 5);
   Rational_t *sum = sumRationalNumbers(num1, num2);
+  Rational_t *sub = subtractRationalNumbers(num1, num2);
 
   for (int i = 0; i < 10; i++) {
     writeRationalNumberInCsv(sum, myFile);
   }
+  printf("original: %d/%d + %d/%d\n", getNumerator(num1), getDenominator(num1), getNumerator(num2), getDenominator(num2));
+  printf("sum: %d/%d\n", getNumerator(sum), getDenominator(sum));
+  printf("sub: %d/%d", getNumerator(sub), getDenominator(sub));
 
   freeRationalNumber(num1);
   freeRationalNumber(num2);
