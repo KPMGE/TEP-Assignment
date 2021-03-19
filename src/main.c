ab23c1043aa8f87960107848e4e82d7ddaad7880
KPMGE <jesuskevin254@gmail.com>
2021-03-19 11:29:35 -0300

    update main program

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "../include/rational-numbers.h"
 #include "../include/complex.h"
 
@@ -7,6 +8,15 @@ int main(void) {
   Complex_t *num = createComplexNumber(1, 4);
 
   displayComplexNumber(num);
+
+  setValueToRealPart(num, sqrt(3));
+  setValueToImaginaryPart(num, 1);
+
+  printf("\nUpdated number:\n");
+  displayComplexNumber(num);
+
+  printf("\nmodule: %.3lf\n", getModuleComplexNumber(num));
+  printf("angle: %.3lf\n", getAngleComplexNumber(num));
 
   freeComplexNumber(num);
 
