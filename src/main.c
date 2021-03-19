0c8b87c59800d8eb65894ec58704030847a52bd1
KPMGE <jesuskevin254@gmail.com>
2021-03-19 16:23:42 -0300

    Update main program

--- src/main.c
+++ src/main.c
@@ -2,23 +2,44 @@
 #include <stdlib.h>
 #include <math.h>
 #include "../include/rational-numbers.h"
-#include "../include/complex.h"
+#include "../include/complex-double.h"
+#include "../include/complex-int.h"
 
 int main(void) {
-  Complex_t *num = createComplexNumber(1, 4);
+  // implementation with long int
+  printf("Implementation with int type:\n\n");
+  INT_Complex_t* num = INT_createComplexNumber(1, 4);
 
-  displayComplexNumber(num);
+  INT_displayComplexNumber(num);
 
-  setValueToRealPart(num, sqrt(3));
-  setValueToImaginaryPart(num, 1);
+  INT_setValueToRealPart(num, sqrt(3));
+  INT_setValueToImaginaryPart(num, 1);
 
   printf("\nUpdated number:\n");
-  displayComplexNumber(num);
+  INT_displayComplexNumber(num);
 
-  printf("\nmodule: %.3lf\n", getModuleComplexNumber(num));
-  printf("angle: %.3lf\n", getAngleComplexNumber(num));
+  printf("\nmodule: %.3lf\n", INT_getModuleComplexNumber(num));
+  printf("angle: %.3lf\n", INT_getAngleComplexNumber(num));
 
-  freeComplexNumber(num);
+  INT_freeComplexNumber(num);
+
+
+  // implementation with double
+  printf("\n\nImplementation with double type:\n\n");
+  DOUBLE_Complex_t* num2 = DOUBLE_createComplexNumber(1, 4);
+
+  DOUBLE_displayComplexNumber(num2);
+
+  DOUBLE_setValueToRealPart(num2, sqrt(3));
+  DOUBLE_setValueToImaginaryPart(num2, 1);
+
+  printf("\nUpdated number:\n");
+  DOUBLE_displayComplexNumber(num2);
+
+  printf("\nmodule: %.3lf\n", DOUBLE_getModuleComplexNumber(num2));
+  printf("angle: %.3lf\n", DOUBLE_getAngleComplexNumber(num2));
+
+  DOUBLE_freeComplexNumber(num2);
 
   return 0;
 }
