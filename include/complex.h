05ebb15517435c11259b88299282ad9b3c916f7e
KPMGE <jesuskevin254@gmail.com>
2021-03-19 16:21:57 -0300

    update complex

--- include/complex.h
+++ include/complex.h
@@ -1,32 +1,30 @@
 #ifndef COMPLEX_H_INCLUDED
 #define COMPLEX_H_INCLUDED
 
-#define TYPE long int
+typedef struct TYPED(complex) TYPED(Complex_t);
 
-typedef struct complex Complex_t;
-
-Complex_t* createComplexNumber(TYPE real, TYPE imag);
-void freeComplexNumber(Complex_t* num);
-void displayComplexNumber(Complex_t* num);
-void assignComplexNumberTo(Complex_t* num);
-void copyComplexNumberTo(Complex_t* num);
+TYPED(Complex_t*) TYPED(createComplexNumber)(TYPE real, TYPE imag);
+void TYPED(freeComplexNumber)(TYPED(Complex_t*) num);
+void TYPED(displayComplexNumber)(TYPED(Complex_t*) num);
+void TYPED(assignComplexNumberTo)(TYPED(Complex_t*) num);
+void TYPED(copyComplexNumberTo)(TYPED(Complex_t*) num);
 
 // we need to create functions to perform conversions
 
 // get/set values into a complex number
-TYPE getRealPart(Complex_t* num);
-TYPE getImaginaryPart(Complex_t* num);
-double getModuleComplexNumber(Complex_t* num);
-double getAngleComplexNumber(Complex_t* num);
-void setValueToRealPart(Complex_t* num, double value);
-void setValueToImaginaryPart(Complex_t* num, double value);
-void setModuleComplex(Complex_t* num); // we need to adjust angle!
-void setAngleComplex(Complex_t* num); // we need to adjust module!
+TYPE TYPED(getRealPart)(TYPED(Complex_t*) num);
+TYPE TYPED(getImaginaryPart)(TYPED(Complex_t*) num);
+double TYPED(getModuleComplexNumber)(TYPED(Complex_t*) num);
+double TYPED(getAngleComplexNumber)(TYPED(Complex_t*) num);
+void TYPED(setValueToRealPart)(TYPED(Complex_t*), double value);
+void TYPED(setValueToImaginaryPart)(TYPED(Complex_t*) num, double value);
+void TYPED(setModuleComplex)(TYPED(Complex_t*) num); // we need to adjust angle!
+void TYPED(setAngleComplex)(TYPED(Complex_t*) num); // we need to adjust module!
 
 // comparison
-int isModuleZero(Complex_t* num);
-int isOnlyRealNumber(Complex_t* num);
-int isOnlyImaginaryNumber(Complex_t* num);
-int compareComplex(Complex_t* num1, Complex_t* num2);
+int TYPED(isModuleZero)(TYPED(Complex_t*) num);
+int TYPED(isOnlyRealNumber)(TYPED(Complex_t*) num);
+int TYPED(isOnlyImaginaryNumber)(TYPED(Complex_t*) num);
+int TYPED(compareComplex)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
 
 #endif
