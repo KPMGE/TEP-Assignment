3a4ff7af919a36f40696dabfd21bf925f212ef10
KPMGE <jesuskevin254@gmail.com>
2021-03-24 12:11:41 -0300

    Update main

--- src/main.c
+++ src/main.c
@@ -10,12 +10,13 @@ int main(void) {
   printf("Implementation with int type:\n\n");
 
   // create numbers
-  INT_Complex_t* num = INT_createComplexNumber(3, 5);
+  INT_Complex_t* num = INT_createComplexNumber(0, 2);
   INT_Complex_t* numTest = INT_createComplexNumber(2, 3);
-  INT_Complex_t *sum, *sub, *multi, *div, *conj;
+  INT_Complex_t *sum, *sub, *multi, *div, *conj, *copy;
 
  
   // display numbers
+  printf("-------------------- ORIGINAL NUMBERS ---------------\n\n");
   printf("num1: ");
   INT_displayComplexNumber(num);
   printf("num2: ");
@@ -23,7 +24,22 @@ int main(void) {
   printf("\n");
 
 
+  printf("\n\n-------------------- IS ONLY REAL/IMAGINARY ---------------\n\n");
+  if (INT_isOnlyRealNumber(num)) {
+    printf("Num1 is only real\n");
+  } else {
+    printf("Num1 is not only real\n");
+  }
+
+  if (INT_isOnlyImaginaryNumber(num)) {
+    printf("Num1 is only imaginary\n");
+  } else {
+    printf("Num1 is not only imaginary\n");
+  }
+
+
   // doing operations
+  printf("\n\n-------------------- OPERATIONS ---------------\n\n");
   // sum
   printf("sum: ");
   sum = INT_evaluateComplexOperation(num, numTest, "+");
@@ -59,10 +75,10 @@ int main(void) {
   printf("num 1 after accumulate multi function: ");
   INT_evaluateComplexOperation(num, numTest, "*=");
   INT_displayComplexNumber(num);
-  printf("\n\n");
 
 
   // testing compare function
+  printf("\n\n-------------------- COMPARISON ---------------\n\n");
   if (INT_compareComplex(num, numTest) == 0) {
     printf("the numbers are the same!");
   } else if (INT_compareComplex(num, numTest) == 1) {
@@ -71,14 +87,26 @@ int main(void) {
     printf("the number 2 is greater than number 1");
   }
 
-  // setting new values
-  INT_setValueToRealPart(num, sqrt(3));
-  INT_setValueToImaginaryPart(num, 1);
-
-  // display updated number
-  printf("\nUpdated number:\n");
+  printf("\n\n-------------------- COPY ---------------\n\n");
+  // copy without modify original values
+  copy = INT_copyComplexNumberTo(num);
+  printf("Copy num1 into an other complex number, by returning it\n");
+  printf("num1: ");
   INT_displayComplexNumber(num);
+  printf("Copy num1: ");
+  INT_displayComplexNumber(copy);
 
+  printf("\n\n-------------------- ASSIGNING ---------------\n\n");
+  // assign numTest into num 
+  INT_assignComplexNumberTo(num, numTest);
+  printf("assign num1 with num2:\n");
+  printf("num1: ");
+  INT_displayComplexNumber(num);
+  printf("num2: ");
+  INT_displayComplexNumber(numTest);
+
+
+  printf("\n\n-------------------- MODULE and ANGLE ---------------\n\n");
   // display module and angle
   printf("\nmodule: %.3lf\n", INT_getModuleComplexNumber(num));
   printf("angle: %.3lf\n", INT_getAngleComplexNumber(num));
@@ -91,6 +119,7 @@ int main(void) {
   INT_freeComplexNumber(multi);
   INT_freeComplexNumber(div);
   INT_freeComplexNumber(conj);
+  INT_freeComplexNumber(copy);
 
   return 0;
 }
