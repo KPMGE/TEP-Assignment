36eacbf84677ae14ccbc1ba64092f5abd73351d8
KPMGE <jesuskevin254@gmail.com>
2021-04-10 22:49:03 -0300

    Adding comments for functions and updating indentation

--- include/complex-rational.h
+++ include/complex-rational.h
@@ -1,42 +1,64 @@
-#undef RATIONAL_INCLUDED
-#undef TYPE
-#undef TYPED
+#ifndef RATIONAL_INCLUDED
 
-#define RATIONAL_INCLUDED
-#define TYPE Rational_t
-#define TYPED(SOMETHING) RATIONAL_ ## SOMETHING
-#include "rational-numbers.h"
+  #undef TYPE
+  #undef TYPED
 
-typedef struct TYPED(complex) TYPED(Complex_t);
+  // define macros for a complex rational number
+  #define RATIONAL_INCLUDED
+  #define TYPE Rational_t
+  #define TYPED(SOMETHING) RATIONAL_ ## SOMETHING
+  #include "rational-numbers.h"
 
-TYPED(Complex_t*) TYPED(createComplexNumber)(TYPE* real, TYPE* imag);
-void TYPED(displayComplexNumber)(TYPED(Complex_t*) num);
-void TYPED(freeComplexNumber)(TYPED(Complex_t*) num);
-TYPE* TYPED(getRealPart)(TYPED(Complex_t*) num);
-TYPE* TYPED(getImaginaryPart)(TYPED(Complex_t*) num);
-void TYPED(setValueToRealPart)(TYPED(Complex_t*), TYPE* value);
-void TYPED(setValueToImaginaryPart)(TYPED(Complex_t*), TYPE* value);
-void TYPED(setModuleComplexNumber)(TYPED(Complex_t*) num, double module);
-void TYPED(setAngleComplexNumber)(TYPED(Complex_t*) num, double angle);
-void TYPED(accumulateComplexSum)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // structure for a rational complex type
+  typedef struct TYPED(complex) TYPED(Complex_t);
 
-TYPED(Complex_t*) TYPED(evaluateComplexOperation)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2, char *op);
+  // function to create a complex rational number
+  TYPED(Complex_t*) TYPED(createComplexNumber)(TYPE* real, TYPE* imag);
+  // function to display a complex rational number
+  void TYPED(displayComplexNumber)(TYPED(Complex_t*) num);
+  // function to free a complex allocated rational number
+  void TYPED(freeComplexNumber)(TYPED(Complex_t*) num);
+  // function to get real part from a complex rational number
+  TYPE* TYPED(getRealPart)(TYPED(Complex_t*) num);
+  // function to get imaginarypart from a complex rational number
+  TYPE* TYPED(getImaginaryPart)(TYPED(Complex_t*) num);
+  // function to set a new value to real part of a complex rational number
+  void TYPED(setValueToRealPart)(TYPED(Complex_t*), TYPE* value);
+  // function to set a new value to imaginary part of a complex rational number
+  void TYPED(setValueToImaginaryPart)(TYPED(Complex_t*), TYPE* value);
+  // function to change module of a complex rational number without modify the orininal angle
+  void TYPED(setModuleComplexNumber)(TYPED(Complex_t*) num, double module);
+  // function to change the angle of a complex rational number without modify your original module
+  void TYPED(setAngleComplexNumber)(TYPED(Complex_t*) num, double angle);
+  // function to accumulate num1 += num2
+  void TYPED(accumulateComplexSum)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // function to calculate module of a complex rational number
+  double TYPED(getModuleComplexNumber)(TYPED(Complex_t*) num);
+  // function to calculate the angle of a complex rational number
+  double TYPED(getAngleComplexNumber)(TYPED(Complex_t*) num);
+  // function to assign numbers num1 = num2 by changing inside them
+  void TYPED(assignComplexNumberTo)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // function to return a copy of a given complex rational number
+  TYPED(Complex_t*) TYPED(copyComplexNumberTo)(TYPED(Complex_t*) num);
+  // check if module of complex number is zero
+  int TYPED(isModuleZero)(TYPED(Complex_t*) num);
+  // check if the complex number has only the real part
+  int TYPED(isOnlyRealNumber)(TYPED(Complex_t*) num);
+  // check if the complex number has only the imaginary part
+  int TYPED(isOnlyImaginaryNumber)(TYPED(Complex_t*) num);
+  // function to compare two complex rational numbers
+  int TYPED(compareComplex)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // function to calculate the conjugate of a complex rational number
+  TYPED(Complex_t*) TYPED(calculateConjugateComplex)(TYPED(Complex_t*) num);
+  // function to sum of two complex numbers
+  TYPED(Complex_t*) TYPED(sumComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // function to subtract of two complex numbers
+  TYPED(Complex_t*) TYPED(subtractComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // function to multiply of two complex numbers
+  TYPED(Complex_t*) TYPED(multiplyComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // function to divide of two complex numbers
+  TYPED(Complex_t*) TYPED(divideComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // function to evaluate operation for a complex rational number
+  TYPED(Complex_t*) TYPED(evaluateComplexOperation)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2, char *op);
 
-
-double TYPED(getModuleComplexNumber)(TYPED(Complex_t*) num);
-double TYPED(getAngleComplexNumber)(TYPED(Complex_t*) num);
-void TYPED(assignComplexNumberTo)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-TYPED(Complex_t*) TYPED(copyComplexNumberTo)(TYPED(Complex_t*) num);
-
-int TYPED(isModuleZero)(TYPED(Complex_t*) num);
-int TYPED(isOnlyRealNumber)(TYPED(Complex_t*) num);
-int TYPED(isOnlyImaginaryNumber)(TYPED(Complex_t*) num);
-int TYPED(compareComplex)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-TYPED(Complex_t*) TYPED(calculateConjugateComplex)(TYPED(Complex_t*) num);
-TYPED(Complex_t*) TYPED(sumComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-TYPED(Complex_t*) TYPED(subtractComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-TYPED(Complex_t*) TYPED(multiplyComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-TYPED(Complex_t*) TYPED(divideComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-
-// conversions
-TYPED(Complex_t*) TYPED(convertIntoComplexRational)(double real, double imag);
+#endif 
--- include/complex.h
+++ include/complex.h
@@ -1,67 +1,61 @@
 #ifndef COMPLEX_H_INCLUDED
-#define COMPLEX_H_INCLUDED
 
-// create a Complext_t for a specific used type
-typedef struct TYPED(complex) TYPED(Complex_t);
+  #define COMPLEX_H_INCLUDED
 
-// create complex number of a given type
-TYPED(Complex_t*) TYPED(createComplexNumber)(TYPE real, TYPE imag);
-// free complex a complex number of a given type
-void TYPED(freeComplexNumber)(TYPED(Complex_t*) num);
-// display a complex number of a given type
-void TYPED(displayComplexNumber)(TYPED(Complex_t*) num);
-// assign a complex number to another one
-void TYPED(assignComplexNumberTo)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-// return a copy of a given complex number
-TYPED(Complex_t*) TYPED(copyComplexNumberTo)(TYPED(Complex_t*) num);
+  // create a Complext_t for a specific used type
+  typedef struct TYPED(complex) TYPED(Complex_t);
 
-
-// get real part of a complex number
-TYPE TYPED(getRealPart)(TYPED(Complex_t*) num);
-// get imaginary part of a complex number
-TYPE TYPED(getImaginaryPart)(TYPED(Complex_t*) num);
-// get module of a complex number
-double TYPED(getModuleComplexNumber)(TYPED(Complex_t*) num);
-// get angle of a complex number
-double TYPED(getAngleComplexNumber)(TYPED(Complex_t*) num);
-// set new value to real part of a complex number
-void TYPED(setValueToRealPart)(TYPED(Complex_t*), double value);
-// set a new value to imaginary part of a complex number
-void TYPED(setValueToImaginaryPart)(TYPED(Complex_t*) num, double value);
-// set a new value to module of a complex number without modify your angle
-void TYPED(setModuleComplexNumber)(TYPED(Complex_t*) num, double k);
-// set a new value to angle of a complex number without modify your module
-void TYPED(setAngleComplexNumber)(TYPED(Complex_t*) num, double angle);
-
-// comparison
-// check if the module of a complex number is zero
-int TYPED(isModuleZero)(TYPED(Complex_t*) num);
-// check if a complex number is actually a real number
-int TYPED(isOnlyRealNumber)(TYPED(Complex_t*) num);
-// check if a complex number is only a imaginary number
-int TYPED(isOnlyImaginaryNumber)(TYPED(Complex_t*) num);
-// compare two complex numbers with respect to module
-int TYPED(compareComplexModule)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-// compare two complex numbers with respect to angle
-int TYPED(compareComplexAngle)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-
-// operations
-// calculate the conjugate of a complex number
-TYPED(Complex_t*) TYPED(calculateConjugateComplex)(TYPED(Complex_t*) num);
-// calculate the sum of two complex numbers
-TYPED(Complex_t*) TYPED(sumComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-// subract complex numbers
-TYPED(Complex_t*) TYPED(subtractComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-// multiply complex numbers
-TYPED(Complex_t*) TYPED(multiplyComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-// divide complex numbers
-TYPED(Complex_t*) TYPED(divideComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-// assign sum of two complex numbers into number1
-void TYPED(accumulateComplexSum)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-// assign multiplication of two complex numbers into number1
-void TYPED(accumulateComplexMulti)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
-
-// function to evalue each operation
-TYPED(Complex_t*) TYPED(evaluateComplexOperation)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2, char *op);
+  // create complex number of a given type
+  TYPED(Complex_t*) TYPED(createComplexNumber)(TYPE real, TYPE imag);
+  // free complex a complex number of a given type
+  void TYPED(freeComplexNumber)(TYPED(Complex_t*) num);
+  // display a complex number of a given type
+  void TYPED(displayComplexNumber)(TYPED(Complex_t*) num);
+  // assign a complex number to another one
+  void TYPED(assignComplexNumberTo)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // return a copy of a given complex number
+  TYPED(Complex_t*) TYPED(copyComplexNumberTo)(TYPED(Complex_t*) num);
+  // get real part of a complex number
+  TYPE TYPED(getRealPart)(TYPED(Complex_t*) num);
+  // get imaginary part of a complex number
+  TYPE TYPED(getImaginaryPart)(TYPED(Complex_t*) num);
+  // get module of a complex number
+  double TYPED(getModuleComplexNumber)(TYPED(Complex_t*) num);
+  // get angle of a complex number
+  double TYPED(getAngleComplexNumber)(TYPED(Complex_t*) num);
+  // set new value to real part of a complex number
+  void TYPED(setValueToRealPart)(TYPED(Complex_t*), double value);
+  // set a new value to imaginary part of a complex number
+  void TYPED(setValueToImaginaryPart)(TYPED(Complex_t*) num, double value);
+  // set a new value to module of a complex number without modify your angle
+  void TYPED(setModuleComplexNumber)(TYPED(Complex_t*) num, double k);
+  // set a new value to angle of a complex number without modify your module
+  void TYPED(setAngleComplexNumber)(TYPED(Complex_t*) num, double angle);
+  // check if the module of a complex number is zero
+  int TYPED(isModuleZero)(TYPED(Complex_t*) num);
+  // check if a complex number is actually a real number
+  int TYPED(isOnlyRealNumber)(TYPED(Complex_t*) num);
+  // check if a complex number is only a imaginary number
+  int TYPED(isOnlyImaginaryNumber)(TYPED(Complex_t*) num);
+  // compare two complex numbers with respect to module
+  int TYPED(compareComplexModule)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // compare two complex numbers with respect to angle
+  int TYPED(compareComplexAngle)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // calculate the conjugate of a complex number
+  TYPED(Complex_t*) TYPED(calculateConjugateComplex)(TYPED(Complex_t*) num);
+  // calculate the sum of two complex numbers
+  TYPED(Complex_t*) TYPED(sumComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // subract complex numbers
+  TYPED(Complex_t*) TYPED(subtractComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // multiply complex numbers
+  TYPED(Complex_t*) TYPED(multiplyComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // divide complex numbers
+  TYPED(Complex_t*) TYPED(divideComplexNumbers)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // assign sum of two complex numbers into number1
+  void TYPED(accumulateComplexSum)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // assign multiplication of two complex numbers into number1
+  void TYPED(accumulateComplexMulti)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2);
+  // function to evalue each operation
+  TYPED(Complex_t*) TYPED(evaluateComplexOperation)(TYPED(Complex_t*) num1, TYPED(Complex_t*) num2, char *op);
 
 #endif
