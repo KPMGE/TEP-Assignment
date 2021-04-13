bf3b6607d08d6d0d5b219ade8f6a3f304f01a4a8
KPMGE <jesuskevin254@gmail.com>
2021-04-13 09:35:24 -0300

    Updating headers

--- include/complex-int.h
+++ include/complex-int.h
@@ -1,11 +1,14 @@
-#undef COMPLEX_H_INCLUDED
-#undef DATA_TYPE
-#undef TYPE_NAME
-#undef IO_FORMAT
-#undef COMPLEX_INT_INCLUDED
+#ifndef COMPLEX_INT_H_INCLUDED
+#define COMPLEX_INT_H_INCLUDED
 
-#define COMPLEX_INT_INCLUDED
-#define DATA_TYPE long int
-#define TYPE_NAME(SOMETHING) INT_ ## SOMETHING
-#define IO_FORMAT "%ld "
-#include "complex.h"
+  #undef COMPLEX_H_INCLUDED
+  #undef DATA_TYPE
+  #undef TYPE_NAME
+  #undef IO_FORMAT
+
+  #define DATA_TYPE long int
+  #define TYPE_NAME(SOMETHING) INT_ ## SOMETHING
+  #define IO_FORMAT "%ld "
+  #include "complex.h"
+
+#endif
