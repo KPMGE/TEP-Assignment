dbf3afe3ff9b105d76aa317c245115b1263ff3d5
KPMGE <jesuskevin254@gmail.com>
2021-04-17 01:56:31 -0300

    testing on main file

--- src/main.c
+++ src/main.c
@@ -13,9 +13,29 @@
 
 
 int main(void) {
+  DOUBLE_Complex_t* num = DOUBLE_createComplexNumber(2.2, 3.1);
+  DOUBLE_Complex_t* num2 = DOUBLE_createComplexNumber(5.5, 9.1);
+  DOUBLE_Complex_t* num3 = DOUBLE_createComplexNumber(34.7, 23.32);
+
+
   DOUBLE_VECT_VectComplex_t* test = DOUBLE_VECT_createVector(10, 0);
-  DOUBLE_displayVector(test);
+  DOUBLE_VECT_insertIndexPosValue(test, num, 0);
+  DOUBLE_VECT_insertIndexPosValue(test, num2, 1);
+  DOUBLE_VECT_insertLastPosValue(test, num3);
+
+  printf("--------- ORIGINAL VECTOR ---------\n");
+  DOUBLE_VECT_displayVector(test);
+
+  printf("\n--------- POSITION 0 DELETED ---------\n");
+  DOUBLE_VECT_deletePosition(test, 0);
+  DOUBLE_VECT_displayVector(test);
+
+
+  DOUBLE_freeComplexNumber(num);
+  DOUBLE_freeComplexNumber(num2);
+  DOUBLE_freeComplexNumber(num3);
   DOUBLE_VECT_freeVector(test);
+
 
   
  // create a vector
