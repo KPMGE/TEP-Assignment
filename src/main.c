2196b7f1d7416252f0731265aa0443e7cb9f9774
KPMGE <jesuskevin254@gmail.com>
2021-04-17 03:00:52 -0300

    Testing on main file

--- src/main.c
+++ src/main.c
@@ -18,23 +18,40 @@ int main(void) {
   DOUBLE_Complex_t* num3 = DOUBLE_createComplexNumber(34.7, 23.32);
 
 
-  DOUBLE_VECT_VectComplex_t* test = DOUBLE_VECT_createVector(10, 0);
-  DOUBLE_VECT_insertIndexPosValue(test, num, 0);
-  DOUBLE_VECT_insertIndexPosValue(test, num2, 1);
-  DOUBLE_VECT_insertLastPosValue(test, num3);
+  DOUBLE_VECT_VectComplex_t* vectTest = DOUBLE_VECT_createVector(10, 0);
+  DOUBLE_VECT_insertIndexPosValue(vectTest, num, 0);
+  DOUBLE_VECT_insertIndexPosValue(vectTest, num2, 1);
+  DOUBLE_VECT_insertLastPosValue(vectTest, num3);
+  DOUBLE_VECT_insertLastPosValue(vectTest, num);
 
   printf("--------- ORIGINAL VECTOR ---------\n");
-  DOUBLE_VECT_displayVector(test);
+  DOUBLE_VECT_displayVector(vectTest);
 
-  printf("\n--------- POSITION 0 DELETED ---------\n");
-  DOUBLE_VECT_deletePosition(test, 0);
-  DOUBLE_VECT_displayVector(test);
+  printf("\n--------- GREATEST MODULE ---------\n");
+  DOUBLE_Complex_t* greatest = DOUBLE_VECT_getHigherAbs(vectTest);
+  DOUBLE_displayComplexNumber(greatest);
+
+  printf("\n\n--------- SMALLEST MODULE ---------\n");
+  DOUBLE_Complex_t* smallest = DOUBLE_VECT_getLowerAbs(vectTest);
+  DOUBLE_displayComplexNumber(smallest);
+
+
+  printf("\n\n--------- AMOUNT EQUALS ---------\n");
+  printf("equals: %d", DOUBLE_VECT_countEquals(vectTest, num));
+  
+  printf("\n\n--------- POSITION 0 DELETED ---------\n");
+  DOUBLE_VECT_deletePosition(vectTest, 0);
+  DOUBLE_VECT_displayVector(vectTest);
+
+  printf("\n--------- CLEARED VECTOR ---------\n");
+  DOUBLE_VECT_clearAllVector(vectTest); 
+  DOUBLE_VECT_displayVector(vectTest);
 
 
   DOUBLE_freeComplexNumber(num);
   DOUBLE_freeComplexNumber(num2);
   DOUBLE_freeComplexNumber(num3);
-  DOUBLE_VECT_freeVector(test);
+  DOUBLE_VECT_freeVector(vectTest);
 
 
   
