71ad8ab0a5a994d472456fc209e434d0b7b260bc
KPMGE <jesuskevin254@gmail.com>
2021-04-17 15:47:06 -0300

    testing on main

--- src/main.c
+++ src/main.c
@@ -8,15 +8,48 @@
 #include "../include/conversions.h"
 #include "../include/vectors-int.h"
 #include "../include/vectors-double.h"
-
 #include "../include/vectors-complex-double.h"
+#include "../include/vectors-complex-int.h"
 
 
 int main(void) {
+  // testing int
+  INT_Complex_t* numInt = INT_createComplexNumber(2, 3);
+  INT_Complex_t* numInt2 = INT_createComplexNumber(4, 7);
+  INT_Complex_t* numInt3 = INT_createComplexNumber(1, 9);
+
+  INT_VECT_VectComplex_t* vectInt = INT_VECT_createVector(5, 0);
+  INT_VECT_insertIndexPosValue(vectInt, numInt, 0);
+  INT_VECT_insertIndexPosValue(vectInt, numInt2, 1);
+  INT_VECT_insertIndexPosValue(vectInt, numInt3, 2);
+
+  printf("--------- ORIGINAL VECTOR INT ---------\n\n");
+  INT_VECT_displayVector(vectInt);
+
+  printf("\n\n--------- MULTPLY BY 3 + 2i ---------\n");
+  INT_Complex_t* complexScalarInt = INT_createComplexNumber(3, 2);
+  INT_VECT_multiplyVectorByScalar(vectInt, complexScalarInt);
+  INT_VECT_displayVector(vectInt);
+
+  printf("\n\n--------- POSITION 0 DELETED ---------\n");
+  INT_VECT_deletePosition(vectInt, 0);
+  INT_VECT_displayVector(vectInt);
+
+  printf("\n--------- CLEARED VECTOR ---------\n");
+  INT_VECT_clearAllVector(vectInt); 
+  INT_VECT_displayVector(vectInt);
+
+  INT_freeComplexNumber(numInt);
+  INT_freeComplexNumber(numInt2);
+  INT_freeComplexNumber(numInt3);
+  INT_freeComplexNumber(complexScalarInt);
+  INT_VECT_freeVector(vectInt);
+
+
+  // create types for double
   DOUBLE_Complex_t* num = DOUBLE_createComplexNumber(2.2, 3.1);
   DOUBLE_Complex_t* num2 = DOUBLE_createComplexNumber(5.5, 9.1);
   DOUBLE_Complex_t* num3 = DOUBLE_createComplexNumber(4.7, 2.32);
-
 
   DOUBLE_VECT_VectComplex_t* vectTest = DOUBLE_VECT_createVector(10, 0);
   DOUBLE_VECT_insertIndexPosValue(vectTest, num, 0);
@@ -24,7 +57,7 @@ int main(void) {
   DOUBLE_VECT_insertLastPosValue(vectTest, num3);
   DOUBLE_VECT_insertLastPosValue(vectTest, num);
 
-  printf("--------- ORIGINAL VECTOR ---------\n");
+  printf("\n\n\n--------- ORIGINAL VECTOR DOUBLE ---------\n");
   DOUBLE_VECT_displayVector(vectTest);
 
   printf("\n--------- GREATEST MODULE ---------\n");
